6c033f41940907cf35ffaa2a1f45dad2dca1206c
jackvdp <jack@pumpymusic.co.uk>
2024-12-05 22:41:52 +0000

    Fix carousel case studies

--- components/CaseStudies/Desktop.tsx
+++ components/CaseStudies/Desktop.tsx
@@ -20,38 +20,41 @@ export default function Desktop({ currentIndex, setCurrentIndex }: {
 
 
     return (
-        <div className="container mx-auto hidden lg:block px-6">
-            <div className="flex w-full mx-auto">
-                <CaseStudyInfo
-                    casestudy={cases[currentIndex]}
-                    scrollPrev={() => api?.scrollPrev()}
-                    scrollNext={() => api?.scrollNext()}
-                />
-                <div className="flex-grow">
-                    <Carousel
-                        setApi={setApi}
-                        opts={{
-                            align: "start",
-                            loop: true,
-                        }}
-                        className="w-full"
-                    >
-                        <CarouselContent className="-ml-4">
-                            {cases.map((case_study, index) => (
-                                <CarouselItem key={index} className="pl-4 basis-[85%]">
-                                    <div className="aspect-[4/3] relative">
-                                        <img
-                                            src={case_study.image}
-                                            alt={case_study.title}
-                                            className="w-full h-full object-cover rounded-2xl"
-                                        />
-                                    </div>
-                                </CarouselItem>
-                            ))}
-                        </CarouselContent>
-                    </Carousel>
+        <div className="overflow-hidden">
+            <div className="container mx-auto hidden lg:block px-6">
+                <div className="flex w-full mx-auto">
+                    <CaseStudyInfo
+                        casestudy={cases[currentIndex]}
+                        scrollPrev={() => api?.scrollPrev()}
+                        scrollNext={() => api?.scrollNext()}
+                    />
+                    <div className="flex-grow">
+                        <Carousel
+                            setApi={setApi}
+                            opts={{
+                                align: "start",
+                                loop: true,
+                            }}
+                            className="w-full"
+                        >
+                            <CarouselContent className="-ml-4">
+                                {cases.map((case_study, index) => (
+                                    <CarouselItem key={index} className="pl-4 basis-[85%]">
+                                        <div className="aspect-[4/3] relative">
+                                            <img
+                                                src={case_study.image}
+                                                alt={case_study.title}
+                                                className="w-full h-full object-cover rounded-2xl"
+                                            />
+                                        </div>
+                                    </CarouselItem>
+                                ))}
+                            </CarouselContent>
+                        </Carousel>
+                    </div>
                 </div>
             </div>
         </div>
+
     )
 }
