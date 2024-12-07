c37f93559a2897abcfc9e6fc64e950c9c43acc1e
jackvdp <jack@pumpymusic.co.uk>
2024-12-07 13:11:42 +0000

    Improve animations for case studies

--- components/CaseStudies/CaseStudyInfo.tsx
+++ components/CaseStudies/CaseStudyInfo.tsx
@@ -1,10 +1,17 @@
 import { CaseStudy } from "./cases";
+import { motion } from "framer-motion";
 
 export default function CaseStudyInfo({ casestudy, scrollPrev, scrollNext }: {
     casestudy: CaseStudy; scrollPrev: () => void; scrollNext: () => void
 }) {
     return (
-        <div className="lg:w-1/3 flex-shrink-0 pr-12 my-auto h-[32rem] flex flex-col">
+        <motion.div
+            initial={{ opacity: 0, x: -40 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            viewport={{ once: true, amount: 0.2 }}
+            transition={{ duration: 0.8 }}
+            className="lg:w-1/3 flex-shrink-0 pr-12 my-auto h-[32rem] flex flex-col"
+        >
             <div className="space-y-6 flex-1 overflow-y-auto">
                 <div className="h-32">
                     <p className="text-sm text-gray-600 mb-2">{casestudy.category}</p>
@@ -40,6 +47,6 @@ export default function CaseStudyInfo({ casestudy, scrollPrev, scrollNext }: {
                     </svg>
                 </button>
             </div>
-        </div>
+        </motion.div>
     )
 }
--- components/CaseStudies/Desktop.tsx
+++ components/CaseStudies/Desktop.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { CarouselApi, Carousel, CarouselContent, CarouselItem } from "@/components/ui/carousel-clip-only-beginning";
 import { cases } from "./cases";
 import CaseStudyInfo from "./CaseStudyInfo";
+import { motion } from "framer-motion";
 
 export default function Desktop({ currentIndex, setCurrentIndex }: {
     currentIndex: number, setCurrentIndex: (index: number) => void
@@ -23,38 +24,55 @@ export default function Desktop({ currentIndex, setCurrentIndex }: {
         <div className="overflow-hidden">
             <div className="container mx-auto hidden lg:block px-6">
                 <div className="flex w-full mx-auto">
+
                     <CaseStudyInfo
                         casestudy={cases[currentIndex]}
                         scrollPrev={() => api?.scrollPrev()}
                         scrollNext={() => api?.scrollNext()}
                     />
-                    <div className="flex-grow">
-                        <Carousel
-                            setApi={setApi}
-                            opts={{
-                                align: "start",
-                                loop: true,
-                            }}
-                            className="w-full"
-                        >
-                            <CarouselContent className="-ml-4">
-                                {cases.map((case_study, index) => (
-                                    <CarouselItem key={index} className="pl-4 basis-[85%]">
-                                        <div className="aspect-[4/3] relative rounded-lg overflow-hidden">
-                                            <img
-                                                src={case_study.image}
-                                                alt={case_study.title}
-                                                className="w-full h-full object-cover hover:scale-105 transition-transform duration-500"
-                                            />
-                                        </div>
-                                    </CarouselItem>
-                                ))}
-                            </CarouselContent>
-                        </Carousel>
-                    </div>
+
+                    <motion.div
+                        initial={{ opacity: 0, x: 20 }}
+                        whileInView={{ opacity: 1, x: 0 }}
+                        viewport={{ once: true, amount: 0.2 }}
+                        transition={{ duration: 0.8 }}
+                    >
+                        <DesktopCarousel apu={api} setApi={setApi} />
+                    </motion.div>
+
+
                 </div>
             </div>
         </div>
 
+    )
+}
+
+function DesktopCarousel({ apu, setApi }: { apu: CarouselApi, setApi: (api: CarouselApi) => void }) {
+    return (
+        <div className="flex-grow">
+            <Carousel
+                setApi={setApi}
+                opts={{
+                    align: "start",
+                    loop: true,
+                }}
+                className="w-full"
+            >
+                <CarouselContent className="-ml-4">
+                    {cases.map((case_study, index) => (
+                        <CarouselItem key={index} className="pl-4 basis-[85%]">
+                            <div className="aspect-[4/3] relative rounded-lg overflow-hidden">
+                                <img
+                                    src={case_study.image}
+                                    alt={case_study.title}
+                                    className="w-full h-full object-cover hover:scale-105 transition-transform duration-500"
+                                />
+                            </div>
+                        </CarouselItem>
+                    ))}
+                </CarouselContent>
+            </Carousel>
+        </div>
     )
 }
