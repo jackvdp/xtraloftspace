8b72b4f6612e4d964a9903810947457178b5fda9
jackvdp <jack@pumpymusic.co.uk>
2024-12-24 18:33:39 +0000

    Make ruler more reusable

--- components/Ruler.tsx
+++ components/Ruler.tsx
@@ -1,5 +1,27 @@
 import {useState, useEffect} from 'react';
 
+function VerticalRuler({position, contentHeight}) {
+    const isRight = position === 'right';
+    const positionClass = isRight ? 'right-16' : 'left-16';
+    const rotateClass = isRight ? 'rotate-90' : '-rotate-90';
+
+    return (
+        <div className={`absolute ${positionClass} top-0 z-1 h-full`} style={{pointerEvents: 'none'}}>
+            <div className="relative h-full">
+                <div className={`absolute ${isRight ? 'left-0' : 'right-0'} h-full w-px bg-gray-300`}></div>
+                {Array.from({length: Math.ceil(contentHeight / 50)}, (_, i) => (i + 1) * 50).map(num => (
+                    <div key={num} className={`absolute ${isRight ? 'left-0' : 'right-0'} flex items-center`}
+                         style={{top: `${num}px`}}>
+                        {!isRight && <span className={`text-[10px] text-gray-300 ${rotateClass}`}>{num}</span>}
+                        <div className="h-px m-1 w-1 bg-gray-300"></div>
+                        {isRight && <span className={`text-[10px] text-gray-300 ${rotateClass}`}>{num}</span>}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
 export default function Ruler() {
     const [contentHeight, setContentHeight] = useState(0);
 
@@ -10,34 +32,13 @@ export default function Ruler() {
 
         updateHeight();
         window.addEventListener('resize', updateHeight);
-
         return () => window.removeEventListener('resize', updateHeight);
     }, []);
 
     return (
         <>
-            <div className="absolute right-16 top-0 z-1 h-full" style={{pointerEvents: 'none'}}>
-                <div className="relative h-full">
-                    {Array.from({length: Math.ceil(contentHeight / 50)}, (_, i) => (i + 1) * 50).map(num => (
-                        <div key={num} className="absolute flex items-center" style={{top: `${num}px`}}>
-                            <div className="h-px m-1 w-1 bg-gray-300"></div>
-                            <span className="text-[10px] text-gray-300 rotate-90">{num}</span>
-                        </div>
-                    ))}
-                    <div className="absolute h-full w-px bg-gray-300"></div>
-                </div>
-            </div>
-            <div className="absolute left-16 top-0 z-1 h-full" style={{pointerEvents: 'none'}}>
-                <div className="relative h-full">
-                    <div className="absolute right-0 h-full w-px bg-gray-300"></div>
-                    {Array.from({length: Math.ceil(contentHeight / 50)}, (_, i) => (i + 1) * 50).map(num => (
-                        <div key={num} className="absolute right-0 flex items-center" style={{top: `${num}px`}}>
-                            <span className="text-[10px] text-gray-300 -rotate-90">{num}</span>
-                            <div className="h-px m-1 w-1 bg-gray-300"></div>
-                        </div>
-                    ))}
-                </div>
-            </div>
+            <VerticalRuler position="left" contentHeight={contentHeight}/>
+            <VerticalRuler position="right" contentHeight={contentHeight}/>
         </>
     );
 }
