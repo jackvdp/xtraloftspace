80bae49201a396ea86c6e65b220d69366b1e8cdd
jackvdp <jack@pumpymusic.co.uk>
2025-01-03 23:45:25 +0000

    Begin adding gallery

--- components/CaseStudies/cases.ts
+++ components/CaseStudies/cases.ts
@@ -6,6 +6,7 @@ export interface CaseStudy {
     shortDescription: string;
     fullDescription: string;
     image: string;
+    folder: string;
 }
 
 export const cases: CaseStudy[] = [
@@ -20,6 +21,7 @@ export const cases: CaseStudy[] = [
 To maintain harmony with the original aesthetic and surrounding properties, we weathered the dormers using natural slate, carefully matching the original slates. Our team managed every aspect of the project, from the initial construction to the final finishing touches. This included crafting bespoke wardrobes, decorating the interiors, hanging a striking wallpaper on the feature wall, and laying high-quality oak-engineered flooring.
 We also took great care to replicate the character of the original staircase, constructing a cut-string staircase and matching all the balustrades. A standout feature of this renovation is the luxurious free-standing bath, positioned thoughtfully in front of the bathroom window, creating a stunning focal point in the en-suite.`,
         image: "/images/cases/eaton-park-road.jpg",
+        folder: "eaton"
     },
     {
         category: "Hip to Gable Loft Conversion",
@@ -32,6 +34,7 @@ We also took great care to replicate the character of the original staircase, co
 A key upgrade involved installing a new heating system for the entire house. We cleverly integrated the boiler and cylinder into a service cupboard within the loft, maximizing functionality and convenience for the homeowners. The loft space was further enhanced with the addition of three electric Velux windows, which flood the area with natural light and add a modern touch.
 We took care of decorating the entire loft and stairwell using paint colours chosen by the customer, ensuring a personalized and cohesive finish. Impressed with the results, the customers entrusted us with further renovations, including a kitchen and bathroom refurbishment downstairs—projects we were delighted to undertake.`,
         image: "/images/cases/hadley-way-hip.jpg",
+        folder: "hadley-loft"
     },
     {
         category: "Refurbishment",
@@ -44,6 +47,7 @@ We took care of decorating the entire loft and stairwell using paint colours cho
 Starting with the bedrooms, we removed the chimney breasts to create additional usable space. The walls were re-drylined, plastered, and freshly decorated to give the rooms a clean and modern look. The upstairs bathroom, in dire need of an upgrade, was refurbished to match the luxurious style of the loft bathroom, using the same high-quality tiles and similar fittings to create a cohesive aesthetic throughout the house.
 On the ground floor, we reconfigured the layout by removing the wall between the dining room and the small kitchen, resulting in a spacious open-plan kitchen diner. We installed a sleek Wren kitchen and laid electric underfloor heating (UFH) in the new kitchen area and hallway. To complete the transformation, we fitted premium Amtico flooring throughout the ground floor, adding a sophisticated and durable finish. Finally, we decorated the entire ground floor, leaving the space ready for the customers to enjoy their beautifully modernized home.`,
         image: "/images/cases/hadley-way-refurb.jpg",
+        folder: "hadley-refurb"
     },
     {
         category: "Rear Extension",
@@ -56,6 +60,7 @@ On the ground floor, we reconfigured the layout by removing the wall between the
 The extension benefits from an abundance of natural light thanks to the glass gable above the sliding doors and Velux windows in the vaulted roof. These elements also provide impressive ceiling height, making the space feel airy and expansive.
 To complement the space, we installed stylish and durable Amtico flooring, adding a premium finish to the new extension. In addition to the main kitchen area, we constructed a downstairs toilet conveniently connected to a new utility room, ensuring practicality without compromising on style. For heating, the new extension is equipped with central heating underfloor heating (UFH), while the existing part of the house was upgraded with elegant new column radiators.`,
         image: "/images/cases/wynchgate-extension.jpg",
+        folder: "whynchgate"
     },
     {
         category: "Hip to Gable Loft Conversion",
@@ -69,6 +74,7 @@ A standout feature of this loft conversion is the 4.5-meter aluminium sliding do
 To preserve the home’s character, we matched the new staircase to the original, ensuring a cohesive flow between the existing house and the loft. Additionally, all cornices in the new loft were meticulously crafted to replicate the original details, adding a touch of elegance to the space.
 The project was completed with a high-quality finish, including decorating throughout and the installation of beautiful oak-engineered flooring, creating a warm and inviting ambiance.`,
         image: "/images/cases/compton-road-loft.jpg",
+        folder: "compton"
     },
     {
         category: "Loft Conversion",
@@ -82,6 +88,7 @@ To facilitate the work, we installed a tin-hat scaffold to protect the property 
 Beyond the loft, we addressed numerous remedial works throughout the property to correct poor workmanship left by the previous builders. These improvements not only restored functionality and aesthetic appeal but also gave the homeowners peace of mind.
 The customer was extremely happy and content with the transformation, finally able to enjoy a safe, beautifully finished home.`,
         image: "/images/cases/maze-green-road.jpg",
+        folder: "bishops"
     },
     {
         category: "Garage Conversion",
@@ -95,6 +102,7 @@ The project involved removing an internal wall and fitting a steel beam to creat
 To complete the transformation, we decorated the space and installed a durable click-system vinyl floor, which added a modern and practical finish. The converted space now serves as a fantastic games room, complete with a custom bar, a pool table, a dartboard, a large TV, and a state-of-the-art sound system. This entertainment hub offers the perfect escape for the homeowners to relax, entertain friends, or watch their favourite sports.
 The customers were thrilled with the transformation and delighted to have a stylish and functional space tailored to their needs.`,
         image: "/images/cases/whitebeam-close-garage.jpg",
+        folder: "whitebeam"
     },
     {
         category: "Loft Conversion",
@@ -108,5 +116,6 @@ The en-suite was fitted with a luxurious four-piece bathroom suite and tiled fro
 The exterior of the dormer was weathered using durable brown concrete plain tiles, complemented by a robust three-layer felt system for the dormer’s flat roof. Additionally, we carried out remedial work on the property’s front dormer, ensuring consistency and durability across all external features.
 The customer was thrilled with the final result, now a spacious and comfortable retreat that perfectly met their needs.`,
         image: "/images/cases/beehive-road-loft.jpg",
+        folder: "beehive"
     },
 ];
