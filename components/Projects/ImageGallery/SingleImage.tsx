267b55cf5d32f0dec586709738fcf047be86cc75
jackvdp <jack@pumpymusic.co.uk>
2025-01-05 11:20:09 +0000

    Improve gallery full view

--- components/Projects/ImageGallery/SingleImage.tsx
+++ components/Projects/ImageGallery/SingleImage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
 import Image from "next/image";
 import {Expand} from "lucide-react";
 
@@ -17,23 +17,33 @@ interface SingleImageProps {
 }
 
 function ExpandButton() {
-    return <div
-        className="
-            absolute
-            inset-0
-            bg-black
-            bg-opacity-0
-            group-hover:bg-opacity-30
-            transition-opacity
-            duration-300
-            flex
-            items-center
-            justify-center
-            pointer-events-none
-          "
-    >
-        <Expand className="text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300"/>
-    </div>;
+    return (
+        <div
+            className="
+        absolute
+        inset-0
+        bg-black
+        bg-opacity-0
+        group-hover:bg-opacity-30
+        transition-opacity
+        duration-300
+        flex
+        items-center
+        justify-center
+        pointer-events-none
+      "
+        >
+            <Expand
+                className="
+          text-white
+          opacity-0
+          group-hover:opacity-100
+          transition-opacity
+          duration-300
+        "
+            />
+        </div>
+    );
 }
 
 function SingleImage({
@@ -44,21 +54,40 @@ function SingleImage({
                          onExpand,
                          onCollapse,
                      }: SingleImageProps) {
+    const containerRef = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        if (expanded && containerRef.current) {
+            const timeoutId = setTimeout(() => {
+                if ("scrollIntoView" in containerRef.current) {
+                    containerRef.current.scrollIntoView({
+                        behavior: "smooth",
+                        block: "center",
+                    });
+                }
+            }, 300);
+
+            return () => clearTimeout(timeoutId);
+        }
+    }, [expanded]);
+
     return (
         <div
+            ref={containerRef}
             data-cursor={expanded ? "close" : "expand"}
             className={`
-            relative 
-            rounded-lg 
-            overflow-hidden 
-            transition-all 
-            duration-300 
-            cursor-pointer
-            ${expanded ? "col-span-full bg-white p-4" : "group h-48 bg-gray-100"}
-            `}
+        relative 
+        rounded-lg 
+        overflow-hidden 
+        transition-all 
+        duration-300 
+        cursor-pointer
+        ${expanded ? "col-span-full bg-white" : "group h-48 bg-gray-100"}
+      `}
             style={{
                 height: expanded ? "80vh" : "12rem",
             }}
+            // 4) Toggle expand/collapse on click
             onClick={() => {
                 if (expanded) {
                     onCollapse();
@@ -74,19 +103,17 @@ function SingleImage({
                 width={1500}
                 height={1000}
                 className={`
-                transition-transform
-                duration-500 
-                w-full 
-                h-full
-                ${expanded ? "object-contain" : "object-cover"}
-                `}
+          transition-transform
+          duration-500
+          w-full
+          h-full
+          ${expanded ? "object-contain" : "object-cover"}
+        `}
                 onLoad={onLoad}
                 priority={index === 0}
             />
 
-            {!expanded && (
-                <ExpandButton/>
-            )}
+            {!expanded && <ExpandButton/>}
         </div>
     );
 }
