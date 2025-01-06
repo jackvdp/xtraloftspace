31b067eb71bb7dcd8cefc5b25cef82be05db0005
jackvdp <jack@pumpymusic.co.uk>
2025-01-06 10:42:11 +0000

    Add favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,47 @@
-import type { Metadata } from "next";
-import { Outfit } from 'next/font/google'
+import type {Metadata} from "next";
+import {Outfit} from 'next/font/google'
 import "./globals.css";
 
 const outfit = Outfit({
-  subsets: ['latin'],
-  // Optional: you can specify custom weights if needed
-  // weight: ['400', '500', '600', '700'],
-  variable: '--font-outfit' // This allows us to use it as a CSS variable
+    subsets: ['latin'],
+    variable: '--font-outfit'
 })
 
 export const metadata: Metadata = {
-  title: "Xtra Loft Space",
-  description: "Leading provider of loft conversions in the UK",
+    icons: {
+        icon: [
+            {url: '/favicon.ico'},
+            {url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png'},
+            {url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png'},
+        ],
+        apple: [
+            {url: '/apple-touch-icon.png'}
+        ],
+        other: [
+            {
+                rel: 'android-chrome-192x192',
+                url: '/android-chrome-192x192.png',
+            },
+            {
+                rel: 'android-chrome-512x512',
+                url: '/android-chrome-512x512.png',
+            },
+        ],
+    },
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${outfit.className} antialiased`}
-      >
+    return (
+        <html lang="en">
+        <body
+            className={`${outfit.className} antialiased`}
+        >
         {children}
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
